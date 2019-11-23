4c39dc1174ef49f3b208692e4cfcf550a8b2f369
hadam <hadam97@naver.co>
2019-11-23 16:59:52 +0900

    index.js 구현

--- union-seminar/routes/home/index.js
+++ union-seminar/routes/home/index.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.use('/foru', require('./foru'));
+router.use('/library', require('./library'));
 
 module.exports = router;
